637cca225839bbd54a46c73678dd4912b86ed1df
Ketan (CometChat) <ketan.yekale@cometchat.com>
2024-12-09 03:36:13 +0530

    Add auth-app-react as a submodule

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import { useEffect } from "react";
-import AuthApp from "auth-app-react"; // Import `auth-app-react`
 
-const App = () => {
+import AuthApp from "./auth-app-react/src/App";
+
+function App(): JSX.Element {
   useEffect(() => {
     // Set a random UID (user1 or user2) in localStorage
     const randomUid = Math.random() < 0.5 ? "user1" : "user2";
     localStorage.setItem("uid", randomUid);
   }, []);
 
-  return (
-    <div>
-      {/* Render the auth-app-react */}
-      <AuthApp />
-    </div>
-  );
-};
+  return <AuthApp />;
+}
 
 export default App;
